0a31f0a366cc8645e311e5c97ac884f17223000f
Munair Simpson <munair@gmail.com>
2015-08-14 09:31:33 +0000

    implementing sounds on link hover

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/img", express.static(__dirname + '/img'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/video", express.static(__dirname + '/video'));
+app.use("/sounds", express.static(__dirname + '/sounds'));
 
 app.get('/', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
 
