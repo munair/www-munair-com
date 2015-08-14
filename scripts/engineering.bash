2cd86a4d32a4924f847a2e9d1374c3eeeb8c06f1
Munair Simpson <munair@gmail.com>
2015-08-14 06:30:22 +0000

    first draft

---
+++ scripts/engineering.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script name : edit.bash
+# script args : $1 -- file to be edited [ALL for working+sub directories]
+#       $2 -- comments for git
+#       $3 -- remove interactivity if parameter equals "noprompting"
+#
+# The script ensures that edits are pushed to the engineering
+# on Heroku. 
+#
+# git remote rm origin
+# git remote add origin https://github.com/munair/engineering-munair-com.git
+
+git init
+if [ $1 == "ALL" ] ; then
+	git add --all .
+else
+	git add $1
+fi
+git commit -m "$2"
+cat ~/.netrc | grep heroku || heroku login && heroku keys:add ~/.ssh/id_rsa.pub
+heroku apps:destroy engineering-munair-com --confirm engineering-munair-com
+heroku apps:create engineering-munair-com
+heroku domains:add engineering.munair.com --app engineering-munair-com
+heroku git:remote -a engineering-munair-com
+git push heroku master
---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/video", express.static(__dirname + '/video'));
+
+app.get('/', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
